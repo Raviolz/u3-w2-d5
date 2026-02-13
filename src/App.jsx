3f8fb1d0b91ecc3d0787b355a0b61bd81c62ac62
Raviolz <gi.ravioli@gmail.com>
2026-02-13 14:31:23 +0100

    multiFetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import WHomepage from "./Components/WHomepage"
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <WNavBar />
-      <WHomepage />
+      <main className="flex-grow-1 d-flex">
+        <WHomepage />
+      </main>
       <WFooter />
-    </>
+    </div>
   )
 }
 
--- src/Components/WHomepage.jsx
+++ src/Components/WHomepage.jsx
@@ -2,55 +2,69 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
+import { useEffect, useState } from "react"
+
+const apiKey = "10006716088e501331dec8ba55214e5f"
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather"
+
+const cities = ["San Francisco,US", "New York,US", "Milan,IT", "Sydney,AU"]
 
 const WHomepage = () => {
+  const [weatherList, setWeatherList] = useState([]) // per settare stato
+
+  useEffect(() => {
+    // per did mount
+
+    // piu fetch insieme con citta' dall array con il map
+    Promise.all(
+      cities.map((city) => {
+        const url = `${baseUrl}?q=${city}&units=metric&lang=en&appid=${apiKey}` // parametro per gradi non in kelvin e lingua
+
+        console.log("Fetch per", city)
+
+        return fetch(url).then((response) => {
+          console.log(response.status)
+
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error("Errore 400")
+          }
+        })
+      }),
+    )
+      .then((dataArray) => {
+        console.log("Tutte e quattro le risposte", dataArray)
+        setWeatherList(dataArray) // PER ME : Occhio che e' un array con 4 risposte oggetto
+      })
+      .catch((error) => {
+        console.log("Errore ", error)
+      })
+  }, [])
+
   return (
-    <Container fluid className="py-5 page-bg">
-      <div className="text-center mb-5">
+    <Container fluid className="py-5 page-bg flex-grow-1">
+      <div className="text-center my-5">
         <h1 className="fw-bold weather-title">Discover the Weather Around the World 🌍</h1>
-        <p className="text-muted fs-5">What's the weather like today in your favorite city?</p>
+        <p className="fw-bold fs-5">What's the weather like today in your favorite city?</p>
       </div>
 
-      <Row className="g-4">
-        <Col md={6} lg={3}>
-          <Card className="weather-card shadow-sm h-100 text-center">
-            <Card.Img variant="top" src="https://placecats.com/300/200" />
-            <Card.Body>
-              <Card.Title>San Francisco</Card.Title>
-              <Card.Text>Coastal breeze and golden sunsets await.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col md={6} lg={3}>
-          <Card className="weather-card shadow-sm h-100 text-center">
-            <Card.Img variant="top" src="https://placecats.com/300/200" />
-            <Card.Body>
-              <Card.Title>New York</Card.Title>
-              <Card.Text>The city that never sleeps — under any sky.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col md={6} lg={3}>
-          <Card className="weather-card shadow-sm h-100 text-center">
-            <Card.Img variant="top" src="https://placecats.com/300/200" />
-            <Card.Body>
-              <Card.Title>Milan</Card.Title>
-              <Card.Text>Style, culture, and changing skies.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col md={6} lg={3}>
-          <Card className="weather-card shadow-sm h-100 text-center">
-            <Card.Img variant="top" src="https://placecats.com/300/200" />
-            <Card.Body>
-              <Card.Title>Sydney</Card.Title>
-              <Card.Text>Sunshine, ocean waves, and open horizons.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+      <Row className="g-4 ">
+        {weatherList.map((cityData) => (
+          <Col md={6} lg={3} key={cityData.id}>
+            <Card className="weather-card shadow-sm h-100 text-center">
+              <Card.Body>
+                <Card.Title className="fw-semibold">{cityData.name}</Card.Title>
+                <div className="fs-2 fw-bold">{Math.round(cityData.main.temp)}°C</div> {/*  arrotondamento */}
+                <Card.Text className="text-capitalize text-muted mb-2">{cityData.weather[0].description}</Card.Text> {/*  ha un array */}
+                <div className="small">
+                  <span className="me-2">Min: {Math.round(cityData.main.temp_min)}°C</span>
+                  <span>Max: {Math.round(cityData.main.temp_max)}°C</span>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   )
